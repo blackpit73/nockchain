55af99a857735f9d897a110cf6ceac45bd24a233
Pit <blackpit73@gmail.com>
2025-06-16 19:57:03 +0200

    hash_hashable_jet

--- crates/zkvm-jetpack/src/hot.rs
+++ crates/zkvm-jetpack/src/hot.rs
@@ -948,23 +948,23 @@ pub const ZTD_JETS: &[HotEntry] = &[
         1,
         hash_noun_varlen_jet,
     ),
-    // (
-    //     &[
-    //         K_138,
-    //         Left(b"one"),
-    //         Left(b"two"),
-    //         Left(b"tri"),
-    //         Left(b"qua"),
-    //         Left(b"pen"),
-    //         Left(b"zeke"),
-    //         Left(b"ext-field"),
-    //         Left(b"misc-lib"),
-    //         Left(b"tip5-lib"),
-    //         Left(b"hash-hashable"),
-    //     ],
-    //     1,
-    //     hash_hashable_jet,
-    // ),
+    (
+        &[
+            K_138,
+            Left(b"one"),
+            Left(b"two"),
+            Left(b"tri"),
+            Left(b"qua"),
+            Left(b"pen"),
+            Left(b"zeke"),
+            Left(b"ext-field"),
+            Left(b"misc-lib"),
+            Left(b"tip5-lib"),
+            Left(b"hash-hashable"),
+        ],
+        1,
+        hash_hashable_jet,
+    ),
 ];
 
 pub const KEYGEN_JETS: &[HotEntry] = &[(
